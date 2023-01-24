975f6faa8b8a56cf90017df3bf5639cf92654b37
Timur Iderisov <timer-1988@mail.ru>
2023-01-25 02:37:25 +0300

    init commit

---
+++ src/test/java/ru/otus/movietelegrambot/MovieTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.movietelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
